2025969a2eb7c6157aceabd7decd1f35b74b88f1
YatriKalathiya <yatri.kalathiyainfotech@gmail.com>
2025-03-28 09:23:10 +0530

    yatri27-3

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function showForgotPassword() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("forgotPasswordForm").style.display = "block";
+}
+
+function showLogin() {
+    document.getElementById("forgotPasswordForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+}
+function togglePassword(fieldId, icon) {
+    let inputField = document.getElementById(fieldId);
+    if (inputField.type === "password") {
+        inputField.type = "text";
+        icon.src = "img/icon/eye-close.png";
+    } else {
+        inputField.type = "password";
+        icon.src = "img/icon/eye.png";
+    }
+}
+function showOtpForm() {
+    document.getElementById("forgotPasswordForm").style.display = "none";
+    document.getElementById("otpForm").style.display = "block";
+}
+
+function showConfirmPassword() {
+    document.getElementById("otpForm").style.display = "none";
+    document.getElementById("ConfirmPassword").style.display = "block";
+}
+
+function moveToNext(input, event) {
+    if (input.value.length === 1) {
+        let next = input.nextElementSibling;
+        if (next) next.focus();
+    }
+}
